8a691425c2fd622426b0a85990ad4c0ae43763a9
Kiss Péter <31178762+kiss-peter@users.noreply.github.com>
2019-08-05 15:52:47 +0200

    Add files via upload

---
+++ Kofejto/Kofejto/Inventory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kofejto
+{
+    class Inventory
+    {
+        public int fa;
+        public int ko;
+        public int vas;
+        public int gyemant;
+
+        public Inventory(int _fa, int _ko, int _vas, int _gyemant)
+        {
+            fa = _fa;
+            ko = _ko;
+            vas = _vas;
+            gyemant = _gyemant;
+        }
+
+    }
+}
