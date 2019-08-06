cd4390d50f5b76b93a089ff855f3937936f0b29f
kiss-peter <petyaking95@gmail.com>
2019-08-06 12:12:04 +0200

    asd

---
+++ Kofejto/Item.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kofejto
+{
+    public class Item
+    {
+        public string nev;
+        public int azonosito;
+        public int[] recept = new int[4]; // 0 - fa, 1 - zuzottko, 2 - vas, 3 - gyemant                    
+        public int sebzes;                                                                                 
+                                                                                                           
+        public Item(int _azonosito, int _sebzes, int[] _recept, string _nev)                                            
+        {
+            nev = _nev;                                                                      
+            azonosito = _azonosito;                                                                        
+            sebzes = _sebzes;                                                                              
+            recept = _recept;                                                                              
+        }                                                                                                  
+        /// <summary>                                                                                      
+        /// Kezdő item                                                                                     
+        /// </summary>                                                                                     
+        /// <param name="_azonosito"></param>                                                              
+        /// <param name="_sebzes"></param>                                                                 
+        public Item(int _azonosito, int _sebzes, string _nev)
+        {
+            nev = _nev;                                                                                                 
+            azonosito = _azonosito;
+            sebzes = _sebzes;
+        }
+
+
+    }
+}
